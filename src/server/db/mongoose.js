34039e42d0169687cdb66fc7ebcb8fe53170811d
CallumCanCreate <callumcancreate@gmail.com>
2019-09-04 22:19:28 +1000

    init commit

---
+++ src/server/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+async function connect() {
+	try {
+		await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+		console.log("Connected to database");
+	} catch (e) {
+		console.log(chalk.red("Error, unable to connect to database"));
+		console.log(e);
+	}
+}
+
+connect();
---
+++ src/server/db/scripts/readSongs.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const Song = require("./Song");
+
+const readSongs = dir => {
+	const data = fs.readFileSync(dir);
+	const rawSongsArray = data
+		.toString()
+		.split(`\r\n`)
+		.map(l => l.split(","));
+	const keys = rawSongsArray[0];
+	let songsArray = [];
+
+	// convert raw data to key value pairs
+	for (let i = 1; i < rawSongsArray.length; i++) {
+		let song = {};
+		for (let j = 0; j < keys.length; j++) {
+			song[keys[j]] = rawSongsArray[i][j];
+		}
+		songsArray.push(song);
+	}
+
+	// parse values
+	songsArray.forEach(song => {
+		for (let val in song) {
+			if (val === "title")
+				song[val] = song[val]
+					.split(" ")
+					.map(word => word[0].toUpperCase() + word.substring(1))
+					.join(" ");
+			if (song[val] === "") song[val] = null;
+			if ((val === "tags" || val === "notes") && song[val])
+				song[val] = song[val].split("&&&");
+			if ((val === "tags" || val === "notes") && !song[val]) song[val] = [];
+			if (val === "author") song[val] = song[val].replace("$$$", ", ");
+		}
+	});
+	console.log(songsArray);
+	return songsArray;
+};
+
+const readAndWriteSongsToJSON = (readDir, writeDir) => {
+	const songs = readSongs(readDir);
+	fs.writeFileSync(writeDir, JSON.stringify(songs));
+};
+
+const readAndWriteSongsToDB = async () => {
+	console.log(process.env.DB_URL);
+	await require("../db/mongoose");
+	const songs = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "/songs.json"))
+	);
+	songs.forEach(s => {
+		const song = new Song(s);
+		song.save();
+	});
+};
+
+// readAndWriteSongsToJSON(
+// 	path.join(__dirname, "/data.csv"),
+// 	path.join(__dirname, "/songs.json")
+// );
+
+readAndWriteSongsToDB();
+
+module.exports = { readSongs, readAndWriteSongsToJSON };
+// console.log(songsArray);
