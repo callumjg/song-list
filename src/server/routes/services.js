ece8353e9d2e5063f7bc2a832b3b001d402a6df9
Callum <callumcancreate@gmail.com>
2019-09-05 22:20:05 +1000

    adding service router

---
+++ src/server/routes/services.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const Service = require("../models/Service");
+
+// create service
+router.post("/", async (req, res) => {
+	try {
+		const service = new Service(req.body);
+		await service.save();
+		res.status(201).send(service);
+	} catch (e) {
+		console.log(e);
+		res.status(400).send(e);
+	}
+});
+
+//Get Service
+router.get("/:_id", async (req, res) => {
+	try {
+		const service = await Service.findById(req.params._id);
+		if (!service) return res.status(404).send();
+		res.send(service);
+	} catch (e) {
+		console.log(e);
+		res.status(400).send(e);
+	}
+});
+
+//Get Services
+router.get("/", async (req, res) => {
+	try {
+		const services = await Service.find({});
+		res.send({ services });
+	} catch (e) {
+		console.log(e);
+		res.status(400).send(e);
+	}
+});
+
+// delete service
+router.delete("/:_id", async (req, res) => {
+	try {
+		const service = await Service.findById(req.params._id);
+		if (!service) return res.status(404).send();
+		await service.remove();
+		res.send(service);
+	} catch (e) {
+		console.log(e);
+		res.status(400).send();
+	}
+});
+
+module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,14 @@ const chalk = require("chalk");
 const server = express();
 const PORT = process.env.PORT || 3000;
 const songsRouter = require("./routes/songs");
+const servicesRouter = require("./routes/services");
 require("./db/mongoose");
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use("/songs", songsRouter);
+server.use("/services", servicesRouter);
 
 server.listen(PORT, () => {
 	console.log(`Server listening on port ${chalk.green(PORT)}`);
