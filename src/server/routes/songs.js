c28eebc3e17859b9b6f99813b19e1b7101a8eabe
Callum <callumcancreate@gmail.com>
2019-10-08 13:08:17 +1000

    further work on songs endpoints

--- src/server/routes/songs.js
+++ src/server/routes/songs.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
 const Song = require("../models/Song");
+const whiteListBody = require("../middleware/whitelistBody");
+
+const allowedUpdates = [
+	"tags",
+	"notes",
+	"title",
+	"url",
+	"author",
+	"key",
+	"tempo",
+	"songSelectID",
+	"tagsAdd",
+	"tagsRemove"
+];
 
 // Create song
 router.post("/", async (req, res) => {
 	try {
 		const song = await new Song(req.body).save();
-		res.send(song);
+		res.send({ song });
 	} catch (e) {
 		let status = e.status || 400;
 		res.status(status).send({ message: e.message });
@@ -40,33 +54,34 @@ router.get("/", async (req, res) => {
 		// options object
 		const options = { sort: { title: 1 } };
 
-		// limit and skip for pagination
+		// set limit and skip for pagination
 		if (req.query.limit) options.limit = parseInt(req.query.limit);
 		if (req.query.skip) options.skip = parseInt(req.query.skip);
 
-		// Handle sort query
+		// set sort options
 		if (req.query.sort) {
 			let parts = req.query.sort.split("_");
 			options.sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
 		}
 
-		// handle search query
+		// set search filter for author or title
 		if (req.query.search) {
-			// search name or title
 			const regex = new RegExp(req.query.search, "i");
 			filter.$or = filter.$or
 				? [...filter.$or, { title: regex }, { author: regex }]
 				: [{ title: regex }, { author: regex }];
-			console.log(regex);
 		}
 
 		// filter by tags
 		if (req.query.tags) {
-			const tags = req.query.tags.split(" ").map(t => t.replace("_", " "));
-			console.log(tags);
+			const tags = req.query.tags
+				.split(" ")
+				.map(t => t.replace("_", " "))
+				.map(t => new RegExp(t, "i"));
+			filter.tags = { $all: tags };
 		}
 
-		// retrieve resource
+		// retrieve and send resource
 		const songs = await Song.find(filter, select, options);
 		res.send({ songs });
 	} catch (e) {
@@ -76,31 +91,44 @@ router.get("/", async (req, res) => {
 });
 
 // Update song
-router.patch("/:_id", async (req, res) => {
-	const whiteListedUpdates = [
-		"tags",
-		"notes",
-		"title",
-		"url",
-		"author",
-		"key",
-		"tempo",
-		"songSelectID"
-	];
-	const updates = Object.keys(req.body);
-	if (updates.length === 0) return res.status(400).send();
-	const validUpdates = updates.filter(u => whiteListedUpdates.includes(u));
-	if (validUpdates.length === 0) return res.status(403).send();
-
+router.patch("/:_id", whiteListBody(allowedUpdates), async (req, res) => {
 	try {
-		const song = await Song.findById(req.params._id);
-		if (!song) return res.status(404).send();
-		validUpdates.forEach(u => (song[u] = req.body[u]));
+		const updates = Object.keys(req.body);
+		const { body, params } = req;
+		const { tagsAdd, tagsRemove } = body;
+
+		if (updates.length === 0) throw new Error("No valid updates provided");
+
+		// Retrieve song and check resource exists
+		const song = await Song.findById(params._id);
+		if (!song) {
+			let e = new Error("Unable to find song");
+			e.status = 404;
+			throw e;
+		}
+
+		// handle new tags
+		if (tagsAdd) {
+			tagsAdd.forEach(t => {
+				let regex = new RegExp(`^${t}$`, "i");
+				if (!song.tags.includes(regex)) song.tags.push(t);
+			});
+			delete req.body.tagsAdd;
+		}
+
+		if (tagsRemove) {
+			tagsRemove.forEach(t => {
+				let regex = new RegExp(`^${t}$`, "i");
+				song.tags = song.tags.filter(st => !st.match(regex));
+			});
+			delete req.body.tagsRemove;
+		}
+		updates.forEach(u => (song[u] = req.body[u]));
 		await song.save();
 		res.send(song);
 	} catch (e) {
-		console.log(e);
-		res.status(400).send();
+		let status = e.status || 400;
+		res.status(status).send({ message: e.message });
 	}
 });
 
@@ -108,12 +136,16 @@ router.patch("/:_id", async (req, res) => {
 router.delete("/:_id", async (req, res) => {
 	try {
 		const song = await Song.findById(req.params._id);
-		if (!song) return res.status(404).send();
+		if (!song) {
+			let e = new Error("Unable to find song");
+			e.status = 404;
+			throw e;
+		}
 		await song.remove();
 		res.send();
 	} catch (e) {
-		console.log(e);
-		res.status(400).send();
+		let status = e.status || 400;
+		res.status(status).send({ message: e.message });
 	}
 });
 
