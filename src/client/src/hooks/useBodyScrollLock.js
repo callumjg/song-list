25b65a6a249acee674552d5eeb36ada0e56236a7
Callum <callumcancreate@gmail.com>
2019-10-29 13:50:02 +1000

    make nav responsive

---
+++ src/client/src/hooks/useBodyScrollLock.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+const useBodyScrollLock = dependency => {
+	const body = useRef(document.querySelector("body")).current;
+	useEffect(() => {
+		if (dependency) {
+			body.style = "overflow:hidden;";
+		}
+		if (!dependency) {
+			body.style = "";
+		}
+		return () => (body.style = "");
+	}, [dependency, body.style]);
+};
+
+export default useBodyScrollLock;
