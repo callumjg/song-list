09fc6fa40311522608f2dea9f2374472e0d80486
callum <callumcancreate@gmail.com>
2020-06-30 22:13:17 +1000

    add auth middleware to protected routes

--- src/server/routes/songs.ts
+++ src/server/routes/songs.ts
@@ -5,7 +5,7 @@ import auth from '../middleware/auth';
 const router = Router();
 
 router.post('/', auth, c.createSong);
-router.get('/metrics', auth, c.getMetrics);
+router.get('/metrics', c.getMetrics);
 router.get('/:songId', c.getSong);
 router.get('/', c.getSongs);
 router.patch('/:songId', auth, c.updateSong);
