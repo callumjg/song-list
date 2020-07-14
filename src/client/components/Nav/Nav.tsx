64f72db1b7be27306a82ba5fb84d68fa690008f3
callum <callumcancreate@gmail.com>
2020-07-15 08:49:24 +1000

    fix nav

--- src/client/components/Nav/Nav.tsx
+++ src/client/components/Nav/Nav.tsx
@@ -1,89 +1,48 @@
-import React, { useState, useContext, useRef } from 'react';
-import CircleMenu from './CircleMenu';
+import React, { useState } from 'react';
+import Hamburger from './Hamburger';
 import useBodyScrollLock from '../../hooks/useBodyScrollLock';
-import { AuthContext } from '../Auth';
-
+import logo from './logo.jpg';
 import './Nav.scss';
 
 export interface Link {
-  onClick: (e?: React.MouseEvent) => void;
+  onClick: () => void;
   label: string;
 }
+
 interface Props {
   links?: Link[];
+  activeTab?: string;
 }
 
-const Nav: React.FC<Props> = ({ links = [] }) => {
+const Nav: React.FC<Props> = ({ links = [], activeTab }) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const navStatus = isNavOpen ? ' open' : '';
-  const { current: refs } = useRef({});
+
   useBodyScrollLock(isNavOpen);
 
-  const onKeyDown = (e) => {
-    if (e.key === 'ArrowDown') refs[0].focus();
-    if (e.key === 'Escape') setIsNavOpen(false);
-  };
-
-  const onLinkKeyDown = (e, i) => {
-    switch (e.key) {
-      case 'Escape':
-        setIsNavOpen(false);
-        break;
-      case 'ArrowDown':
-        if (i === links.length - 1) {
-          refs[0].focus();
-        } else {
-          refs[i + 1].focus();
-        }
-        break;
-      case 'ArrowUp':
-        if (i === 0) {
-          refs[links.length - 1].focus();
-        } else {
-          refs[i - 1].focus();
-        }
-        break;
-      default:
-      // no default
-    }
-  };
   return (
-    <div>
-      <div>
-        <CircleMenu
-          isOpen={isNavOpen}
-          onClick={() => setIsNavOpen(!isNavOpen)}
-          onKeyDown={onKeyDown}
-        />
-      </div>
-      <nav>
-        <div
-          className={`nav-container${navStatus}`}
-          onClick={() => setIsNavOpen(false)}
-          onKeyPress={(e) => {
-            console.log('test');
-            if (e.key === 'Enter') setIsNavOpen(false);
-          }}
-          role="none"
-        >
-          <div className={`nav-slider${navStatus}`}>
+    <nav className="navbar navbar-light bg-light">
+      <img src={logo} className="navbar-brand" alt="GPC" />
+      <Hamburger onClick={() => setIsNavOpen(true)} />
+      <div
+        className={`links-slider-backdrop${navStatus}`}
+        onClick={() => setIsNavOpen(false)}
+      >
+        <div className={`links-slider${navStatus}`}>
+          <ul className="navbar-nav">
             {links.map(({ onClick, label }, i) => (
-              <button
-                key={label}
-                type="button"
+              <li
+                key={i}
                 onClick={onClick}
-                ref={(ref) => {
-                  refs[i] = ref;
-                }}
-                onKeyDown={(e) => onLinkKeyDown(e, i)}
+                className={activeTab === label ? 'nav-link active' : 'nav-link'}
               >
                 {label}
-              </button>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
