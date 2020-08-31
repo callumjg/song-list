86a6d2a8d8a7b810e0f968bd36394bf53eab498d
callum <callumcancreate@gmail.com>
2020-08-31 15:19:50 +1000

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,116 +5,141 @@ const NodeExternals = require('webpack-node-externals');
 const Nodemon = require('nodemon-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopmressionPlugin = require('compression-webpack-plugin');
-
 const src = path.resolve(__dirname, 'src');
+const { merge } = require('webpack-merge');
 
-const makeConfig = (name) => (env) => {
-  const isServer = name === 'server';
+const shared = (env) => {
   const isProduction = !!env.production;
-  const sharedPlugins = [
-    new webpack.IgnorePlugin(/\.\/locale$/, /moment$/),
-    new CleanWebpackPlugin(),
-  ];
-  let plugins = isServer
-    ? [
-        ...sharedPlugins,
-        new Nodemon({
-          script: './dist/server.js',
-          watch: path.resolve(__dirname, './dist'),
-          nodeArgs: ['-r', 'dotenv/config'],
-        }),
-      ]
-    : [
-        ...sharedPlugins,
-        new HtmlWebpackPlugin({
-          template: __dirname + '/src/server/public/index.html',
-          favicon: __dirname + '/src/server/public/favicon.ico',
-          filename: 'template.html',
-        }),
-      ];
-
-  if (isProduction && !isServer) {
-    plugins = [
-      ...plugins,
-      new CopmressionPlugin({
-        filename: '[path].br[query]',
-        algorithm: 'brotliCompress',
-        test: /\.(js|css|html|svg)$/,
-        compressionOptions: { level: 11 },
-        threshold: 10240,
-        minRatio: 0.8,
-        cache: true,
-      }),
-      new CopmressionPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8,
-        cache: true,
-        deleteOriginalAssets: true,
-      }),
-    ];
-  }
-
-  const rules = [
-    {
-      test: /\.tsx?$/,
-      include: src,
-      use: ['ts-loader'],
-      include: src,
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.jsx?$/,
-      include: src,
-      use: ['babel-loader'],
-      include: src,
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.(s[ac]|c)ss$/i,
-      use: isServer
-        ? 'ignore-loader'
-        : ['style-loader', 'css-loader', 'sass-loader'],
-    },
-    {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: 'file-loader',
-      include: src,
-      exclude: /node_modules/,
-    },
-  ];
-
-  if (!isProduction) {
-    rules.push({
-      enforce: 'pre',
-      test: /\.js$/,
-      use: 'source-map-loader',
-    });
-    rules[0].use = [...rules[0].use, 'eslint-loader'];
-  }
 
   return {
     mode: isProduction ? 'production' : 'development',
-    entry: {
-      [name]: `${__dirname}/src/${name}/${isServer ? 'index.ts' : 'index.tsx'}`,
-    },
-    output: {
-      filename: '[name].js',
-      path: isServer ? __dirname + '/dist/' : __dirname + '/dist/public/',
-      publicPath: '/',
-    },
-    module: {
-      rules,
-    },
     devtool: isProduction ? undefined : 'source-maps',
-    plugins,
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
       symlinks: false,
       cacheWithContext: false,
     },
+    node: {
+      __dirname: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: src,
+          use: isProduction ? 'ts-loader' : ['ts-loader', 'eslint-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.jsx?$/,
+          use: ['babel-loader'],
+          include: src,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: 'file-loader',
+          include: src,
+          exclude: /node_modules/,
+        },
+        ...(isProduction
+          ? []
+          : [
+              {
+                enforce: 'pre',
+                test: /\.js$/,
+                use: 'source-map-loader',
+                include: src,
+                exclude: /node_modules/,
+              },
+            ]),
+      ],
+    },
+  };
+};
+
+const server = (env) => {
+  const config = {
+    name: 'server',
+    entry: `${__dirname}/src/server/index.ts`,
+    output: {
+      filename: 'server.js',
+      path: __dirname + '/dist/',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(s[ac]|c)ss$/i,
+          use: 'ignore-loader',
+        },
+      ],
+    },
+    externals: [new NodeExternals()],
+    target: 'node',
+    plugins: [
+      new Nodemon({
+        script: './dist/server.js',
+        watch: path.resolve(__dirname, './dist'),
+        nodeArgs: ['-r', 'dotenv/config'],
+      }),
+      new webpack.IgnorePlugin(/\.\/locale$/, /moment$/),
+      new CleanWebpackPlugin(),
+    ],
+  };
+
+  return merge(shared(env), config);
+};
+
+const client = (env) => {
+  const isProduction = !!env.production;
+  const config = {
+    name: 'client',
+    entry: `${__dirname}/src/client/index.tsx`,
+    output: {
+      filename: 'client.js',
+      path: __dirname + '/dist/public/',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(s[ac]|c)ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    target: 'web',
+    plugins: [
+      new webpack.IgnorePlugin(/\.\/locale$/, /moment$/),
+      new HtmlWebpackPlugin({
+        template: __dirname + '/src/server/public/index.html',
+        favicon: __dirname + '/src/server/public/favicon.ico',
+        filename: 'template.html',
+      }),
+      ...(isProduction
+        ? [
+            new CopmressionPlugin({
+              filename: '[path].br[query]',
+              algorithm: 'brotliCompress',
+              test: /\.(js|css|html|svg)$/,
+              compressionOptions: { level: 11 },
+              threshold: 10240,
+              minRatio: 0.8,
+              cache: true,
+            }),
+            new CopmressionPlugin({
+              filename: '[path].gz[query]',
+              algorithm: 'gzip',
+              test: /\.js$|\.css$|\.html$/,
+              threshold: 10240,
+              minRatio: 0.8,
+              cache: true,
+              deleteOriginalAssets: true,
+            }),
+          ]
+        : []),
+    ],
     optimization: {
       splitChunks: {
         cacheGroups: {
@@ -126,12 +151,10 @@ const makeConfig = (name) => (env) => {
         },
       },
     },
-    externals: isServer ? [new NodeExternals()] : [],
-    target: isServer ? 'node' : 'web',
-    node: {
-      __dirname: true,
-    },
   };
+  return merge(shared(env), config);
 };
 
-module.exports = [makeConfig('server'), makeConfig('client')];
+module.exports = [client, server];
+
+// module.exports = [makeConfig('server'), makeConfig('client')];
